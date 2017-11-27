898b242880cd04507e3ce4770422bdfe8453d39e
aeolus <thetesseractsshadow@gmail.com>
2017-11-26 20:09:06 -0500

    tile class added

--- tile.py
+++ tile.py
@@ -1,8 +1,14 @@
 import entity
+import sprite_pack
 
 
 class Tile(entity.Entity):
     def __init__(self, surface, rect):
-        super(Tile, self).__init__()
+        super(Tile, self).__init__(surface, rect)
+
+        self.image = sprite_pack.target
+
+    def render(self):
+        self.surface.blit(self.image, (self.x, self.y))
 
 
