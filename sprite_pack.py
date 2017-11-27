10b87f9e162083df4f236c2f2a9eded8fb7a5443
aeolus <thetesseractsshadow@gmail.com>
2017-11-26 20:09:20 -0500

    tile class added

--- sprite_pack.py
+++ sprite_pack.py
@@ -5,6 +5,7 @@ import constants
 slime = pygame.image.load("spritesheet_large.png")
 cube = pygame.image.load("cube_spritesheet.png")
 cube2 = pygame.image.load("cube_spritesheet2.png")
+target = pygame.image.load("target.png")
 
 cube_pack = (cube, 3, constants.FPS/7)  # image surface, number of frames (starts at 0), frame rate
 cube_pack2 = (cube2, 3, constants.FPS/7)
